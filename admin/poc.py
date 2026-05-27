92040b7e5603447663e2d7bd30eed71da84ad42a
Shiva Goli <sgoli@paloaltonetworks.com>
2026-05-27 16:45:54 -0700

    update

--- admin/poc.py
+++ admin/poc.py
@@ -3,8 +3,6 @@ import requests
 TARGET_URL = "http://localhost:8080/login"
 
 def exploit():
-    # The attacker is trying to bypass authentication by passing an operator
-    # that evaluates to true. However, this is MongoDB syntax, NOT SQLite syntax.
     payload = {
         "username": {"$gt": ""},
         "password": {"$gt": ""}
